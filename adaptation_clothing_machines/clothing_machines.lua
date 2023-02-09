620f24a1cdee5b1afd7783459d6900abf2f5f533
SFENCE <sfence.software@gmail.com>
2023-02-09 17:43:36 +0100

    Make adaptation_clothing_machines loadable.

--- adaptation_clothing_machines/clothing_machines.lua
+++ adaptation_clothing_machines/clothing_machines.lua
@@ -1,5 +1,5 @@
 
 adaptation_lib.add_item("needle_bone", {
-    name = "clothing_machones:bone_needle",
+    name = "clothing_machines:bone_needle",
   })
 
--- adaptation_clothing_machines/init.lua
+++ adaptation_clothing_machines/init.lua
@@ -2,6 +2,6 @@
 local modpath = minetest.get_modpath(minetest.get_current_modname())
 
 if minetest.get_modpath("clothing_machines") then
-  dofile(modpath.."/machines.lua")
+  dofile(modpath.."/clothing_machines.lua")
 end
 
